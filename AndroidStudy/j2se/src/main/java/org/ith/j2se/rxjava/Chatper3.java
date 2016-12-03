1ae61248a1a418d50be625f9df05cb25676b0dd0
tanghao <tangxiansheng21@gmail.com>
2016-12-03 22:44:49 +0800

    then a chain of filter map things goes filter map filter map NOT ... filter filter map map ...

--- AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/Chatper3.java
+++ AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/Chatper3.java
@@ -2,10 +2,24 @@ package org.ith.j2se.rxjava;
 
 import rx.Observable;
 
-
 public class Chatper3 {
   public static void main(String[] args) {
 
+    test0();
+  }
+
+  public static void test0() {
+    /**
+     * things goes
+     *
+     * filter map
+     * filter map
+     * ...
+     * NOT
+     * filter filter filter
+     * map map map
+     * ...
+     */
     Observable.just(8, 9, 10)
         .filter(i -> i % 3 > 0)
         .doOnNext(it -> System.out.println("filter --> " + it))
@@ -13,6 +27,5 @@ public class Chatper3 {
         .doOnNext(it -> System.out.println("map #  --> " + it))
         .filter(s -> s.length() < 4)
         .forEach(System.out::println);
-
   }
 }
