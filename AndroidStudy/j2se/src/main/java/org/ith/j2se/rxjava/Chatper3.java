771efde41cc4131662a12945c108e0c797c11d6b
tanghao <tangxiansheng21@gmail.com>
2016-12-04 20:54:23 +0800

    map one to one flatmap one to many many-->Observable<xxx>

--- AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/Chatper3.java
+++ AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/Chatper3.java
@@ -1,16 +1,64 @@
 package org.ith.j2se.rxjava;
 
+import rx.Observable;
+
+import static org.ith.j2se.rxjava.Chatper3.Sound.DAH;
+import static org.ith.j2se.rxjava.Chatper3.Sound.DI;
 import static rx.Observable.empty;
 import static rx.Observable.just;
 
 public class Chatper3 {
   public static void main(String[] args) {
 
-    test0();
+    test3();
+  }
+
+//-----------------------test3------------------------------//
+
+  /***
+   * as a rule of thumb,you use flatMap for the following situations:
+   *
+   * 1.The Result of transformation in map must be an Observable.
+   *
+   * 2.You need a one-to-many transformation.
+   *
+   * a single event is expanded into multtiple sub-events.
+   */
+  public static void test3() {
+    Observable.just('A', 'B', 'C')
+        .map(Character::toLowerCase)
+        .flatMap(it -> toMorseCode(it));
+  }
+
+  public static Observable<Sound> toMorseCode(char sh) {
+    switch (sh) {
+      case 'A':
+        return just(DI, DAH);
+      case 'B':
+        return just(DAH, DI, DI, DI);
+      case 'C':
+        return just(DAH, DI, DAH, DI);
+      //...
+      case 'P':
+        return just(DI, DAH, DAH, DI);
+      case 'R':
+        return just(DI, DAH, DI);
+      case 'S':
+        return just(DI, DI, DI);
+      case 'T':
+        return just(DAH);
+
+      default:
+        return empty();
+    }
   }
 
   /**
    * flatmap replace filter or map
+   *
+   * in practice,we do not replace map() and filter() with flatMap()
+   *
+   * due to the clarity of code and performance
    */
   public static void test1() {
     //----------filter --> flatmap------------//
@@ -30,6 +78,8 @@ public class Chatper3 {
     //----------map --> flatmap------------//
   }
 
+//-----------------------test3------------------------------//
+
   /**
    * things goes
    *
@@ -48,4 +98,7 @@ public class Chatper3 {
         .filter(s -> s.length() < 4)
         .forEach(System.out::println);
   }
+
+  enum Sound {DI, DAH}
+
 }
