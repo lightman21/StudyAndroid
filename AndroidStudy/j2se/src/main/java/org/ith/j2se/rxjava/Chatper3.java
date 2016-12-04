1ed0a65b8818be7d93531f781688bc26674ba6f8
tanghao <tangxiansheng21@gmail.com>
2016-12-04 14:57:42 +0800

    how to use flatmap replace filter or map

--- AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/Chatper3.java
+++ AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/Chatper3.java
@@ -1,6 +1,7 @@
 package org.ith.j2se.rxjava;
 
-import rx.Observable;
+import static rx.Observable.empty;
+import static rx.Observable.just;
 
 public class Chatper3 {
   public static void main(String[] args) {
@@ -8,19 +9,38 @@ public class Chatper3 {
     test0();
   }
 
+  /**
+   * flatmap replace filter or map
+   */
+  public static void test1() {
+    //----------filter --> flatmap------------//
+    just(10)
+        .filter(x -> x != 10);
+    //equivalent to
+    just(10)
+        .flatMap(x -> x != 10 ? just(x) : empty());
+    //----------filter --> flatmap------------//
+
+
+    //----------map --> flatmap------------//
+    just(10)
+        .map(x -> x * 2);
+    //equivalent
+    just(10).flatMap(x -> just(x * 2));
+    //----------map --> flatmap------------//
+  }
+
+  /**
+   * things goes
+   *
+   * filter map filter map ...
+   *
+   * NOT
+   *
+   * filter filter filter map map map ...
+   */
   public static void test0() {
-    /**
-     * things goes
-     *
-     * filter map
-     * filter map
-     * ...
-     * NOT
-     * filter filter filter
-     * map map map
-     * ...
-     */
-    Observable.just(8, 9, 10)
+    just(8, 9, 10)
         .filter(i -> i % 3 > 0)
         .doOnNext(it -> System.out.println("filter --> " + it))
         .map(i -> "#" + i * 10)
