0c0acfde15f8aa38ebf7b35dc9ddf3e69bb27d85
tanghao <tangxiansheng21@gmail.com>
2016-12-07 08:07:49 +0800

    introduce startwith for dummy event

--- AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/chapter3/ZipProblem.java
+++ AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/chapter3/ZipProblem.java
@@ -1,5 +1,6 @@
 package org.ith.j2se.rxjava.chapter3;
 
+import java.util.concurrent.TimeUnit;
 import rx.Observable;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -10,49 +11,36 @@ import static rx.Observable.interval;
  *
  * 1.problem: when zip two Observables has different event frequency
  *
- * 1.solution:combineLatest
+ * 1.solution:combineLatest() operator -----> solveZipDiffFrequency()
  *
- * the Observable trigger each other which means when slower event appears,
- *
- * it will find the lasted faster event.
- *
- * and when faster appears.it  also find the lasted slower event
+ * the Observable trigger each other which means when slower event appears, it will find the lasted
+ * faster event. and when faster appears.it  also find the lasted slower event
  *
  *
  * 2.problem: but what if you just want A Observable trigger B Observable and get the lastest event
  * but not vice versa.
  *
- * 2.solutioin: withLatestFrom()   Rx 1.0.1 --> 1.2.3
+ * 2.solutioin: withLatestFrom() -----> solveTrigerEachOther()
+ *
+ * Rx 1.0.1 --> 1.2.3
+ *
+ *
+ *
+ * 3.problem: but when you use withlastestFrom. slow event appeare before the fast event so there
+ * are some events from slow event will be droped. because at beginning only slow event emitted and
+ * there are no fast event emitted. so the slow event will be droped.
+ *
+ * 3.solution: startswith() -----> solveDropedEvent()
+ *
+ * startswith()  generate some dummy event
  */
 public class ZipProblem {
   public static void main(String[] args) {
 
+    solveZipDiffFrequency();
     solveTrigerEachOther();
+    solveDropedEvent();
 
-  }
-
-  public static void solveTrigerEachOther() {
-    Observable<String> fast = interval(10, MILLISECONDS).map(x -> "F" + x);
-    Observable<String> slow = interval(17, MILLISECONDS).map(x -> "S" + x);
-    slow.withLatestFrom(fast, (s, f) -> s + ":" + f)
-        .forEach(System.out::println);
-
-
-    /**
-     * Output:
-     *
-     * as you see,something missed in the faster Observable
-     * but the slower Observable goes ok.
-     *
-     S0:F1
-     S1:F2
-     S2:F4
-     S3:F5
-     S4:F7
-     S5:F9
-     S6:F11
-     *
-     */
   }
 
   public static void solveZipDiffFrequency() {
@@ -93,6 +81,56 @@ public class ZipProblem {
      F1001:S588
      */
   }
+
+  public static void solveTrigerEachOther() {
+    Observable<String> fast = interval(10, MILLISECONDS).map(x -> "F" + x);
+    Observable<String> slow = interval(17, MILLISECONDS).map(x -> "S" + x);
+    slow.withLatestFrom(fast, (s, f) -> s + ":" + f)
+        .forEach(System.out::println);
+
+
+    /**
+     * Output:
+     *
+     * as you see,something missed in the faster Observable
+     * but the slower Observable goes ok.
+     *
+     S0:F1
+     S1:F2
+     S2:F4
+     S3:F5
+     S4:F7
+     S5:F9
+     S6:F11
+     *
+     */
+  }
+
+  public static void solveDropedEvent() {
+    Observable<String> fast = interval(10, MILLISECONDS).map(x -> "F" + x)
+        .delay(100, TimeUnit.MILLISECONDS).startWith("FX");
+
+    Observable<String> slow = interval(17, MILLISECONDS).map(x -> "S" + x);
+
+    slow.withLatestFrom(fast, (s, f) -> s + ":" + f)
+        .forEach(System.out::println);
+
+    /**
+     * output:
+
+     S0:FX
+     S1:FX
+     S2:FX
+     S3:FX
+     S4:FX
+     S5:FX
+     S6:F1
+     S7:F3
+     S8:F4
+     S9:F6
+     */
+  }
+
 
   public static void base(long leftFrequency, int rightFrequency) {
     Observable<Long> red = interval(leftFrequency, MILLISECONDS);
