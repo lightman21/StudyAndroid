4684fac8b25a7bf004b4bcbfeacaa6054ea79a2b
tanghao <tangxiansheng21@gmail.com>
2016-12-21 11:33:38 +0800

    Subscriptions.create() and Subscriber.add() things

---
+++ AndroidStudy/j2se/src/main/java/org/ith/j2se/rxjava/chapter2/LazyTwitterObservable.java
@@ -0,0 +1,100 @@
+package org.ith.j2se.rxjava.chapter2;
+
+import com.annimon.stream.Stream;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import rx.Observable;
+import rx.Subscriber;
+import rx.subscriptions.Subscriptions;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+/**
+ * Created by tanghao on 12/21/16.
+ *
+ * If Manually managing Subscribers
+ */
+public class LazyTwitterObservable {
+
+  private final Set<Subscriber<? super Status>> subscribers = new CopyOnWriteArraySet<>();
+
+  private final TwitterStream twitterStream;
+
+  private final Observable<Status> observalbe = Observable.create(
+      subscriber -> {
+
+        register(subscriber);
+
+        subscriber.add(Subscriptions.create(() ->
+            this.deregister(subscriber)
+        ));
+
+        //Subscriptons.create(Action0 unsubscribe)
+
+        //public static Subscription create(final Action0 unsubscribe) {
+        //  return BooleanSubscription.create(unsubscribe);
+        //}
+      }
+  );
+
+  public LazyTwitterObservable() {
+    this.twitterStream = new TwitterStreamFactory().getInstance();
+    this.twitterStream.addListener(new StatusListener() {
+      @Override
+      public void onStatus(Status status) {
+        Stream.of(subscribers).forEach(s -> s.onNext(status));
+      }
+
+      @Override
+      public void onException(Exception e) {
+        Stream.of(subscribers).forEach(s -> s.onError(e));
+      }
+
+      @Override
+      public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+
+      }
+
+      @Override
+      public void onTrackLimitationNotice(int i) {
+
+      }
+
+      @Override
+      public void onScrubGeo(long l, long l1) {
+
+      }
+
+      @Override
+      public void onStallWarning(StallWarning stallWarning) {
+
+      }
+
+    });
+  }
+
+  Observable<Status> observe() {
+    return observalbe;
+  }
+
+  private synchronized void register(Subscriber<? super Status> subscriber) {
+    if (subscribers.isEmpty()) {
+      subscribers.add(subscriber);
+      twitterStream.sample();
+    } else {
+      subscribers.add(subscriber);
+    }
+  }
+
+  private synchronized void deregister(Subscriber<? super Status> subscriber) {
+    subscribers.remove(subscriber);
+
+    if (subscribers.isEmpty()) {
+      twitterStream.shutdown();
+    }
+  }
+}
